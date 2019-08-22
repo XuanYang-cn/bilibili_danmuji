7084194448bd3e50e61e8c942b4df57594b70694
Yang Xuan <jumpthepig@gmail.com>
2019-08-22 20:06:41 +0800

    adding checkin

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import time
 import threading
 import requests
@@ -10,10 +11,16 @@ class Danmu():
     MSG_URL = 'https://api.live.bilibili.com/ajax/msg'
 
     def __init__(self, roomid: str, interval=1):
-        self._tasks = Queue()
-        self._unique = OrderedDict()
-        self.roomid = str(roomid)
-        self.interval = interval
+        self.roomid = str(roomid)  # 直播房间号
+        self.interval = interval  # 间隔多少秒爬取一次弹幕， 默认为1
+
+        #  self._filepath = os.path.join(os.path.abspath('.'), self.roomid+'.txt')
+        #  if not os.path.isfile(self._filepath):
+        #      with open(self._filepath, 'w') as f:
+        #          pass
+        self._tasks = Queue()  # 需要依次打印的弹幕
+        self._checkin_task = Queue()  # 需要记录的打卡弹幕
+        self._unique = OrderedDict()  # 存储最近10个新弹幕
         self._headers = {
             "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36",
@@ -21,6 +28,7 @@ class Danmu():
         }
 
     def check(self):
+        '''Check 直播间的状态是否正常'''
         payload = {'roomid': self.roomid}
         with requests.Session() as session:
             res = session.get(url=self.CHECK_URL, headers=self._headers, params=payload)
@@ -28,6 +36,7 @@ class Danmu():
             print(f'Successfully Checked! Roomid = {self.roomid}')
 
     def get_danmu(self):
+        '''单线程实时获取弹幕，去除重复弹幕'''
         payload = {
                 'roomid': self.roomid,
                 'csrf_token': '',
@@ -56,14 +65,30 @@ class Danmu():
             time.sleep(self.interval)
 
     def print_worker(self):
+        '''顺序打印所有弹幕'''
         while True:
-            time.sleep(0.5)
             item = self._tasks.get()
 
             if item:
                 # Colored: bilibili pink
                 nickname = '\033[35m' + item['nickname']+'\033[0m'
-                print(f"[{item['timeline']}] {nickname}: {item['text']}")
+                text = item['text']
+                print(f"[{item['timeline']}] {nickname}: {text}")
+
+                # TODO re
+                if text == "打卡":
+                    self._checkin_task.put(item)
+
+    def check_in_worker(self):
+        '''打印打卡成功'''
+        while True:
+            item = self._checkin_task.get()
+
+            if item:
+                nickname = '\033[35m' + item['nickname']+'\033[0m'
+                print(f"[{item['timeline']}] {nickname} \033[94m打卡成功\033[0m")
+
+
 
     def run(self):
         t1 = threading.Thread(target=self.print_worker)
@@ -71,12 +96,16 @@ class Danmu():
 
         t1.start()
         t2.start()
+        t3.start()
+        # TODO these threads will never join
         t1.join()
         t2.join()
+        t3.join()
 
 
 if __name__ == '__main__':
     roomid = input("请输入房间号: ").strip()
-    yx = Danmu(roomid)
+    # 7734200
+    yx = Danmu(roomid, interval=0.2)
     yx.check()
     yx.run()
