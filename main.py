1c7794f96a435c7aa25700576d8382bedf9003e1
Yang Xuan <jumpthepig@gmail.com>
2019-08-23 15:08:39 +0800

    repeating checkin is not allowed now

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ from queue import Queue
 from collections import OrderedDict
 
 
+class Color:
+    BILIBILI_PINK = '\033[35m'
+    GREEN = '\033[94m'
+    ENDC = '\033[0m'
+
+
 class Danmu():
     CHECK_URL = 'https://api.live.bilibili.com/lottery/v1/Storm/check'
     MSG_URL = 'https://api.live.bilibili.com/ajax/msg'
@@ -14,10 +20,7 @@ class Danmu():
         self.roomid = str(roomid)  # 直播房间号
         self.interval = interval  # 间隔多少秒爬取一次弹幕， 默认为1
 
-        #  self._filepath = os.path.join(os.path.abspath('.'), self.roomid+'.txt')
-        #  if not os.path.isfile(self._filepath):
-        #      with open(self._filepath, 'w') as f:
-        #          pass
+        self._checkin_list = {}
         self._tasks = Queue()  # 需要依次打印的弹幕
         self._checkin_task = Queue()  # 需要记录的打卡弹幕
         self._unique = OrderedDict()  # 存储最近10个新弹幕
@@ -71,13 +74,17 @@ class Danmu():
 
             if item:
                 # Colored: bilibili pink
-                nickname = '\033[35m' + item['nickname']+'\033[0m'
+                nickname = Color.BILIBILI_PINK + item['nickname'] + Color.ENDC
                 text = item['text']
                 print(f"[{item['timeline']}] {nickname}: {text}")
 
                 # TODO re
                 if text == "打卡":
-                    self._checkin_task.put(item)
+                    if item['uid'] not in self._checkin_list:
+                        self._checkin_task.put(item)
+                        self._checkin_list.update({item['uid']: True})
+                    else:
+                        print(f"[{item['timeline']}] {nickname} 今天已打卡，请勿重复打卡")
 
     def check_in_worker(self):
         '''打印打卡成功'''
@@ -85,25 +92,26 @@ class Danmu():
             item = self._checkin_task.get()
 
             if item:
-                nickname = '\033[35m' + item['nickname']+'\033[0m'
-                print(f"[{item['timeline']}] {nickname} \033[94m打卡成功\033[0m")
-
-
+                nickname = Color.BILIBILI_PINK + item['nickname'] + Color.ENDC
+                print(f"[{item['timeline']}] {nickname} {Color.GREEN}打卡成功{Color.ENDC}")
 
     def run(self):
         t1 = threading.Thread(target=self.print_worker)
         t2 = threading.Thread(target=self.get_danmu_worker)
+        t3 = threading.Thread(target=self.check_in_worker)
 
         t1.start()
         t2.start()
+        t3.start()
         # TODO these threads will never join
         t1.join()
         t2.join()
+        t3.join()
 
 
 if __name__ == '__main__':
     roomid = input("请输入房间号: ").strip()
     # 7734200
-    yx = Danmu(roomid, interval=0.2)
+    yx = Danmu(roomid)
     yx.check()
     yx.run()
